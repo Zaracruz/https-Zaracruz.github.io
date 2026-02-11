4432002c33980c3c2f37fb56e69f0e4d6aac6f04
Zaracruz <C00285103@setu.ie>
2026-02-11 15:42:18 +0000

    fixing sw

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,11 @@
-const CACHE_NAME = "my-app-v1";
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/Settings.html",
-  "/style.css",
-  "/script.js",
-  "/manifest.json",
-  "/icons/home.png",
-  "/icons/settings.png",
-  "/icons/dropdown.png"
-];
-
-// Install – cache files
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
-  );
+self.addEventListener("install", event => {
+  self.skipWaiting();
 });
 
-// Activate – clean old caches
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) =>
-      Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      )
-    )
-  );
+self.addEventListener("activate", event => {
+  event.waitUntil(clients.claim());
 });
 
-// Fetch – serve from cache first
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => response || fetch(event.request))
-  );
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request));
 });
